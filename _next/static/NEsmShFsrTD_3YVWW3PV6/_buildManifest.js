5cd6bc6223b8bf03de803158faf70443c328625c
tsots <mosalatsotetsi@gmail.com>
2022-07-21 16:01:35 +0200

    Deploy

--- _next/static/NEsmShFsrTD_3YVWW3PV6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-cf664ac30935d3cf.js"],"/_error":["static/chunks/pages/_error-b53b8f599aff85eb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/x8sHeXTdrH4O2xi3HWR96/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-cf664ac30935d3cf.js"],"/_error":["static/chunks/pages/_error-b53b8f599aff85eb.js"],"/about":["static/chunks/pages/about-586ea400bb98ac98.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
