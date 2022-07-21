5d236fbbe497e9ce9b91b9c87499c11cfc96a0c8
tsots <mosalatsotetsi@gmail.com>
2022-07-21 16:00:27 +0200

    _app.js changed

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,22 @@ import { ThemeProvider } from "styled-components";
 
 import { Theme } from "../styles/Theme";
 import { Global } from "styles/Global";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={Theme}>
       <Global />
+      <div>
+        <ul>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </ul>
+      </div>
       <Component {...pageProps} />
     </ThemeProvider>
   );
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const about = () => {
+  return (
+    <>
+      <h1>about</h1>
+      <p>What About heeee?</p>
+    </>
+  );
+};
+
+export default about;
