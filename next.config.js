62cf83e80116f2023a5d221b43b2909656a26136
tsots <mosalatsotetsi@gmail.com>
2022-07-21 17:17:51 +0200

    for vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-const debug = process.env.NODE_ENV !== "production";
+// const debug = process.env.NODE_ENV !== "production";
 
-module.exports = {
-  // basePath: '/crisp',
-  basePath: !debug ? "/climb" : "",
+// module.exports = {
+//   // basePath: '/crisp',
+//   basePath: !debug ? "/climb" : "",
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 };
+
+module.exports = nextConfig;
