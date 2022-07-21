bd41673730cab16d5d071214ec0ccc36f7bc4afb
tsots <mosalatsotetsi@gmail.com>
2022-07-21 16:22:55 +0200

    Deploy

---
+++ out/_next/static/pXe4ItSigM4a6W-02MuNV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-cf664ac30935d3cf.js"],"/_error":["static/chunks/pages/_error-b53b8f599aff85eb.js"],"/about":["static/chunks/pages/about-f0d85669d0ff8185.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
